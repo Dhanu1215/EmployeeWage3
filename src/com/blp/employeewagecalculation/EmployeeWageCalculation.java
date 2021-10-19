a95a814e53d392b29e98a9a070b0784331172e38
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-19 23:07:27 +0530

    Added: Welcome to Employee wage computation program message File is added

---
+++ src/com/blp/employeewagecalculation/EmployeeWageCalculation.java
@@ -0,0 +1,17 @@
+package com.blp.employeewagecalculation;
+
+/**
+ * Employee Wage Calculation Program
+ * Check Employee Is Present Or Absent
+ */
+public class EmployeeWageCalculation {
+
+    public static void main(String[] args) {
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        if ( empCheck == 1 ) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
