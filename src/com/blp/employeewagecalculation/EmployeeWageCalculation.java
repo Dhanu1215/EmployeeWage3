b0217e252ac0dcf802f1004758369a54ef622a95
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-19 23:40:39 +0530

    Added: Employee part time wage calculation file is added

--- src/com/blp/employeewagecalculation/EmployeeWageCalculation.java
+++ src/com/blp/employeewagecalculation/EmployeeWageCalculation.java
@@ -4,22 +4,30 @@ package com.blp.employeewagecalculation;
  * Employee Wage Calculation Program
  * Check Employee Is Present Or Absent
  * Calculate Daily Wage
+ * Part Time Employee Wage
  */
 public class EmployeeWageCalculation {
+    //Declaration of Constant variables
+    public static final int EMP_FULL_TIME_HOUR = 1;
+    public static final int EMP_PART_TIME_HOUR = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        //Use random function to check employee is present or absent
+        //Declaration of variables
         int empHours;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        if ( empCheck == 1 ) {
+        //Use random function to check employee is present or absent
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        if ( empCheck == EMP_FULL_TIME_HOUR ) {
             empHours = 8;
             System.out.println("Employee is Present");
+        } else if ( empCheck == EMP_PART_TIME_HOUR ) {
+            empHours = 4;
+            System.out.println("Employee is Part time present");
         } else {
             empHours = 0;
             System.out.println("Employee is Absent");
         }
         int empWage = (EMP_WAGE_PER_HOUR * empHours);
-        System.out.println("Employee Daily Wage = " + empWage);
+        System.out.println("Employee Wage = " + empWage);
     }
 }
